cbef1fb94bd1e3a2f150efb0569b2d5173ecb02a
Joshitha-03 <kunigantijoshitha@gmail.com>
2024-09-21 12:54:59 +0530

    code inserted

--- first.js
+++ first.js
@@ -1,2 +1,73 @@
-let btn = document.querySelectorAll(".box");
-let tic0 =true;
+let boxes = document.querySelectorAll(".box");
+let msg = document.querySelector(".msg");
+let msgcontainer =document.querySelector(".msgcontainer");
+let reset =document.querySelector("#reset");
+let newgame = document.querySelector("#newgame");
+let turn0 =true;
+let winpattern = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+let count=0;
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+      if(turn0){
+        box.innerHTML="O";
+        turn0=false;
+      }
+      else{
+        box.innerHTML="X";
+        turn0=true;
+      }
+      box.disabled="true";
+      checkwinner();
+    })
+})
+const checkwinner =()=>{
+    for(pattern of winpattern){
+        let pos1= boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3= boxes[pattern[2]].innerText;
+
+        if(pos1!=="" && pos2!=="" &&pos3!=""){
+            if(pos1===pos2&&pos2===pos3){
+                showwinner(pos1);
+            }
+            else{
+                  count++;
+            }
+        }
+    }
+    if(count===8){
+        msgcontainer.classList.remove("hide");
+       msg.innerText="Game is draw, please take new game";
+    }
+}
+const disable=()=>{
+    for(box of boxes){
+        box.disabled=true;
+       }
+}
+const enable=()=>{
+    for(box of boxes){
+        box.disabled=false;
+       }
+}
+newgame.addEventListener("click",()=>{
+    msgcontainer.classList.add("hide");
+    for(box of boxes){
+        box.innerText="";
+    }
+    turn0=true;
+     enable();
+})
+reset.addEventListener("click",()=>{
+    msgcontainer.classList.add("hide");
+    for(box of boxes){
+        box.innerText="";
+    }
+    turn0=true;
+     enable();
+})
+const showwinner=(pos1)=>{
+     disable();
+       msgcontainer.classList.remove("hide");
+       msg.innerText=`Congratulations, Winner is player ${pos1}`;
+}
